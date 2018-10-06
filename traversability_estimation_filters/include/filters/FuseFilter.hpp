3a545addbb1368c697da790a1c62ae719b6a99a4
Stefan Kohlbrecher <kohlbrecher@sim.tu-darmstadt.de>
2018-10-06 15:35:46 +0200

    Add fuse filter

---
+++ traversability_estimation_filters/include/filters/FuseFilter.hpp
@@ -0,0 +1,71 @@
+/*
+ * StepFilter.hpp
+ *
+ *  Created on: Mar 12, 2015
+ *      Author: Martin Wermelinger
+ *   Institute: ETH Zurich, Autonomous Systems Lab
+ */
+
+#ifndef FUSEFILTER_HPP
+#define FUSEFILTER_HPP
+
+#include <filters/filter_base.h>
+
+#include <string>
+
+namespace filters {
+
+/*!
+ * Step Filter class to compute the step traversability value of an elevation map.
+ */
+template<typename T>
+class FuseFilter : public FilterBase<T>
+{
+
+ public:
+  /*!
+   * Constructor
+   */
+  FuseFilter();
+
+  /*!
+   * Destructor.
+   */
+  virtual ~FuseFilter();
+
+  /*!
+   * Configures the filter from parameters on the Parameter Server
+   */
+  virtual bool configure();
+
+  /*!
+   * Computes the step traversability value based on an elevation map and
+   * saves it as additional grid map layer.
+   * The step traversability is set between 0.0 and 1.0, where a value of 1.0 means fully
+   * traversable and 0.0 means not traversable. NAN indicates unknown values (terrain).
+   * @param mapIn grid map containing elevation map and surface normals.
+   * @param mapOut grid map containing mapIn and step traversability values.
+   */
+  virtual bool update(const T& mapIn, T& mapOut);
+
+ private:
+
+  //! Maximum allowed step depth.
+  double max_allowed_step_depth_;
+
+  //! Window sizes for step filter
+  //double firstWindowRadius_, secondWindowRadius_;
+
+  //! Distance in cells looking for negative steps
+  int sample_distance_cells_;
+
+  //! Critical number of cells greater than maximums allowed step.
+  int nCellCritical_;
+
+  //! Step map type.
+  std::string type_;
+};
+
+} /* namespace */
+
+#endif
---
+++ traversability_estimation_filters/src/FuseFilter.cpp
@@ -0,0 +1,130 @@
+/*
+ * StepFilter.cpp
+ *
+ *  Created on: Mar 11, 2015
+ *      Author: Martin Wermelinger
+ *   Institute: ETH Zurich, Autonomous Systems Lab
+ */
+
+#include "filters/FuseFilter.hpp"
+#include <pluginlib/class_list_macros.h>
+#include <algorithm>
+
+// Grid Map
+#include <grid_map_ros/grid_map_ros.hpp>
+
+using namespace grid_map;
+
+namespace filters {
+
+template<typename T>
+FuseFilter<T>::FuseFilter()
+    : max_allowed_step_depth_(0.15),
+      sample_distance_cells_(10),
+      //firstWindowRadius_(0.08),
+      //secondWindowRadius_(0.08),
+      nCellCritical_(3),
+      type_("traversability")
+{
+
+}
+
+template<typename T>
+FuseFilter<T>::~FuseFilter()
+{
+
+}
+
+template<typename T>
+bool FuseFilter<T>::configure()
+{
+  if (!FilterBase<T>::getParam(std::string("max_allowed_step_depth"), max_allowed_step_depth_)) {
+    ROS_ERROR("Step filter did not find param max_allowed_step_depth.");
+    return false;
+  }
+
+  if (max_allowed_step_depth_ < 0.0) {
+    ROS_ERROR("Max allowed step depth must be greater than zero.");
+    return false;
+  }
+
+  ROS_DEBUG("Max allowed step depth = %f.", max_allowed_step_depth_);
+
+  if (!FilterBase<T>::getParam(std::string("sample_distance_cells"),
+                               sample_distance_cells_)) {
+    ROS_ERROR("Step filter did not find param 'sample_distance_cells'.");
+    return false;
+  }
+
+  if (sample_distance_cells_ < 0) {
+    ROS_ERROR("'sample_distance_cells' must be greater than zero.");
+    return false;
+  }
+
+  if (!FilterBase<T>::getParam(std::string("critical_cell_number"),
+                               nCellCritical_)) {
+    ROS_ERROR("Step filter did not find param 'critical_cell_number'.");
+    return false;
+  }
+
+  if (nCellCritical_ <= 0) {
+    ROS_ERROR("'critical_cell_number' must be greater than zero.");
+    return false;
+  }
+
+  ROS_DEBUG("Number of critical cells of step filter = %d.", nCellCritical_);
+
+  if (!FilterBase<T>::getParam(std::string("map_type"), type_)) {
+    ROS_ERROR("Step filter did not find param map_type.");
+    return false;
+  }
+
+  ROS_DEBUG("Negative Step map type = %s.", type_.c_str());
+
+  return true;
+}
+
+template<typename T>
+bool FuseFilter<T>::update(const T& mapIn, T& mapOut)
+{
+
+
+  mapOut = mapIn;
+  mapOut.add(type_);
+
+  const grid_map::Matrix& traversability_slope_data = mapOut["traversability_slope"];
+  const grid_map::Matrix& traversability_step_data = mapOut["traversability_step"];
+  const grid_map::Matrix& traversability_negative_step = mapOut["traversability_negative_step"];
+  const grid_map::Matrix& traversability_roughness_data = mapOut["traversability_roughness"];
+
+  grid_map::Matrix& traversability_data = mapOut[type_];
+
+  // First iteration through the elevation map.
+  for (GridMapIterator iterator(mapOut); !iterator.isPastEnd(); ++iterator) {
+
+    //grid_map::wrapIndexToRange(cindex, mapOut.getSize());
+
+    //const float curr_elevation_data = elevation_data(cindex.x(), cindex.y());
+    //std::cout << " el: " << curr_elevation_data << "\n";
+
+
+
+    if (!mapOut.isValid(*iterator, "elevation"))
+      continue;
+
+    grid_map::Index curr_index(*iterator);
+
+    traversability_data(curr_index.x(), curr_index.y()) = std::min({
+                                                            traversability_slope_data(curr_index.x(), curr_index.y()),
+                                                            traversability_step_data(curr_index.x(), curr_index.y()),
+                                                            traversability_negative_step(curr_index.x(), curr_index.y()),
+                                                            traversability_roughness_data(curr_index.x(), curr_index.y()),
+                                                           });
+  }
+  return true;
+}
+
+} /* namespace */
+
+PLUGINLIB_REGISTER_CLASS(FuseFilter, filters::FuseFilter<grid_map::GridMap>,
+                         filters::FilterBase<grid_map::GridMap>)
